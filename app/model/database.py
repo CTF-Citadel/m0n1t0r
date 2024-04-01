047ec76228f50864f3c3016f53754e899c4b4ca4
Aryt3 <Jxkobus@protonmail.com>
2024-04-01 20:25:48 +0000

    adding dynamic dbhost via envvar

--- app/model/database.py
+++ app/model/database.py
@@ -5,8 +5,9 @@ import os
 db_user = os.environ.get("DB_USER")
 db_password = os.environ.get("DB_PW")
 db_name = os.environ.get("DB_NAME")
+db_host = os.environ.get("DB_HOST")
 
-SQLALCHEMY_DB_URL = f'postgresql://{db_user}:{db_password}@db:5432/{db_name}'
+SQLALCHEMY_DB_URL = f'postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}'
 
 engine = create_engine(SQLALCHEMY_DB_URL, echo=True)
 DBSession = sessionmaker(engine, autoflush=False)
